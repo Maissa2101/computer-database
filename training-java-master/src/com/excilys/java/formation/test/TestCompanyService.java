27c9801dd66b19cdddcf8c78702e23d80ded7a06
epheng <excilys@localhost.localdomain>
2018-03-14 17:57:58 +0100

    Javadoc

---
+++ training-java-master/src/com/excilys/java/formation/test/TestCompanyService.java
@@ -0,0 +1,19 @@
+package com.excilys.java.formation.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCompanyService {
+
+	@Test
+	public void testGetCompanyService() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListCompanies() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ training-java-master/src/com/excilys/java/formation/test/TestComputerService.java
@@ -0,0 +1,39 @@
+package com.excilys.java.formation.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestComputerService {
+
+	@Test
+	public void testGetComputerService() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListComputers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputerDetails() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateComputer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateComputer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteComputer() {
+		fail("Not yet implemented");
+	}
+
+}
