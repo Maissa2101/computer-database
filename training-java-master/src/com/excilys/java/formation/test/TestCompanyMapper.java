09efb2c3e27a3c495d1755c7c7c1bd8d710dcc49
epheng <excilys@localhost.localdomain>
2018-03-15 14:10:58 +0100

    Creation of TestCompanyMapper

---
+++ training-java-master/src/com/excilys/java/formation/test/TestCompanyMapper.java
@@ -0,0 +1,14 @@
+package com.excilys.java.formation.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCompanyMapper {
+
+	@Test
+	public void testGetListCompanyFromResultSet() {
+		fail("Not yet implemented");
+	}
+
+}
