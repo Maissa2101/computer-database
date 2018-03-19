800d71f3afa9206d32333885aaff64e8b32d72bc
epheng <excilys@localhost.localdomain>
2018-03-19 16:49:36 +0100

    [Test] hsql test

--- src/test/java/com/excilys/java/formation/persistence/SQLConnectionTest.java
+++ src/test/java/com/excilys/java/formation/persistence/SQLConnectionTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -11,29 +12,47 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.excilys.java.formation.persistence.SQLConnection;
-
 
 public class SQLConnectionTest {
 		
-	 @SuppressWarnings("unused")
+	
 	@BeforeClass
 	     public static void init() throws SQLException, ClassNotFoundException, IOException {
-	         SQLConnection.getInstance();
-			 Connection con = SQLConnection.getConnection();
+		 	Class.forName("org.hsqldb.jdbc.JDBCDriver");
+		 	initDatabase();
 	     }
 
 	
 	 @AfterClass
 	     public static void destroy() throws SQLException, ClassNotFoundException, IOException {
-	         try (Connection connection = SQLConnection.getConnection()){
-	             connection.close();
-	         }
-	     } 
+		 	try (Connection connection = getConnection(); java.sql.Statement statement = connection.createStatement();) {
+			             statement.executeUpdate("DROP TABLE company");
+			             connection.commit();
+		 	} 
+	 	}
 	 
-	 	
+	 private static void initDatabase() throws SQLException {
+		         try (Connection connection = getConnection(); java.sql.Statement statement = connection.createStatement();) {
+		             statement.execute("CREATE TABLE company (id BIGINT NOT NULL identity, name VARCHAR(255),"
+		                     + "constraint pk_computer PRIMARY KEY (id));");
+		             connection.commit();
+		             statement.executeUpdate("INSERT INTO company (name) VALUES ('Apple Inc.');");
+		             statement.executeUpdate("INSERT INTO company (name) VALUES ('Thinking Machines');");
+		             statement.executeUpdate("INSERT INTO company (name) VALUES ('RCA');");
+		             connection.commit();
+		         }
+		     }
+
+	 
+
+	 
+	 	private static Connection getConnection() throws SQLException {
+		         return DriverManager.getConnection("jdbc:hsqldb:mem:testdb", "sa", "");
+		     }
+
+	 
 	 	private int getTotalRecords() {
-			 try (Connection connection = SQLConnection.getConnection(); java.sql.Statement statement = connection.createStatement();) {
+			 try (Connection connection = getConnection(); java.sql.Statement statement = connection.createStatement();) {
 				 ResultSet result = statement.executeQuery("SELECT count(*) as total FROM company");
 			     	if (result.next()) {
 			     		return result.getInt("total");
@@ -46,19 +65,19 @@ public class SQLConnectionTest {
 		  
 		@Test
 		public void getTotalRecordsTest() {
-			assertEquals(42, getTotalRecords());
+			assertEquals(3, getTotalRecords());
 		}
 		  
 		@Test
 		 public void checkNameExistsTest() {
-			try (Connection connection = SQLConnection.getConnection();
+			try (Connection connection = getConnection();
 					java.sql.Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);) {
 					ResultSet result = statement.executeQuery("SELECT name FROM company");
 					if (result.first()) {
 						assertEquals("Apple Inc.", result.getString("name"));
 		             }
 		            if (result.last()) {
-		            	assertEquals("Samsung Electronics", result.getString("name"));
+		            	assertEquals("RCA", result.getString("name"));
 		             }
 		    } catch (SQLException e) {
 		             e.printStackTrace();
